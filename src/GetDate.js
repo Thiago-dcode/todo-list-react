47d396f1aee9ed3ba3583a638b117ce19f0dbe79
thiago <contact.thiago.ferreira@gmail.com>
2022-06-11 17:04:49 +0200

    css style

--- src/GetDate.js
+++ src/GetDate.js
@@ -6,7 +6,7 @@ export const GetDate = () => {
     const minute = date.getMinutes()
 
 
-    const fullDate = `${date.toDateString()} ${hour}:${minute}`
+    const fullDate = `${date.toDateString()} ${hour}:${minute <10 ? '0' + minute:minute}`
     return fullDate
 
 }
